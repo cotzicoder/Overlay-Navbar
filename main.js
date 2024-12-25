dd48ab13c7bf874d86813087959b1e8e7f927827
CotziCoder <cotzicoder@gmail.com>
2024-12-25 17:24:31 +0530

    Overlay navbar
    navbar
    
    Co-Authored-By: CotziCoder <192525960+cotzicoder@users.noreply.github.com>

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('navbar').style.height = "0%"
-function close11() {
-    document.getElementById('navbar').style.height = "0%"
-}
-
-function open11() {
-    document.getElementById('navbar').style.height = "100%"
-}
