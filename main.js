41990a5cfd5ced376b1ce1022dda3cae88ea196c
CotziCoder <cotzicoder@gmail.com>
2024-12-26 18:41:02 +0530

    POP UP USING CSS & JS
    POP UP USING CSS & JS

---
+++ main.js
@@ -0,0 +1,12 @@
+let contentmenu = document.getElementById('content-menu');
+contentmenu.style.display = "none";
+
+function Opencontentmenu() {
+    let contentmenu = document.getElementById('content-menu');
+    contentmenu.style.display = "block";
+}
+
+function closecontentmenu() {
+    let contentmenu = document.getElementById('content-menu');
+    contentmenu.style.display = "none";
+}
